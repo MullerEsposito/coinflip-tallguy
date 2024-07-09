61a3303d65fcc6785c8026a0e6d8ce95ac78c4fd
jonandgon <jonathan@puzzle.online>
2024-07-09 11:05:02 +0200

    update the button

--- src/components/CoinFlipper.js
+++ src/components/CoinFlipper.js
@@ -6,16 +6,19 @@ import { EventType, requestCreateEvent, shortenAddress, useAccount, useConnect }
 const CoinFlipper = () => {
   const [isFlipping, setIsFlipping] = useState(false);
   const [result, setResult] = useState(null);
+  const [visualResult, setVisualResult] = useState('heads');
+
   const [event, setEvent] = useState(null);
-  const { connect, isConnected, loading } = useConnect()
+  const { connect, isConnected, loading } = useConnect();
   const { account } = useAccount();
 
   const flipCoin = () => {
     setIsFlipping(true);
+    const newResult = Math.random() < 0.5 ? 'heads' : 'tails';
+    setVisualResult(newResult);
     setResult(null);
-    setEvent(null)
+    setEvent(null);
     setTimeout(async () => {
-      const newResult = Math.random() < 0.5 ? 'heads' : 'tails';
       const event = await requestCreateEvent({
         fee: 1,
         functionId: 'flip',
@@ -55,7 +58,7 @@ const CoinFlipper = () => {
     <>
       <button
         className="fixed-button"
-        onClick={connect}
+        onClick={() => connect().then(() => console.log('connected').catch).catch((e) => console.error(e))}
       >
         {buttonText}
       </button>
@@ -63,16 +66,38 @@ const CoinFlipper = () => {
         <h1 className="comic-title">PUZZLE COIN FLIP</h1>
         <div className="coin-container">
           <div className={`coin ${isFlipping ? 'flipping' : ''}`}>
-            <div className="coin-face heads">HEADS</div>
-            <div className="coin-face tails">TAILS</div>
+            {visualResult === 'tails' ? (
+                <>
+                  <div className="coin-face tails">TAILS</div>
+                  <div className="coin-face heads bottom">HEADS</div>
+                </>
+              ) : (
+                <>
+                  <div className="coin-face heads">HEADS</div>
+                  <div className="coin-face tails bottom">TAILS</div>
+                </>
+              )
+            }
           </div>
         </div>
-        <button className="comic-button" onClick={flipCoin} disabled={isFlipping || !isConnected}>
+        <button className="comic-button" onClick={() => {
+          console.log('clicked', isConnected);
+
+          if (isConnected) {
+            flipCoin()
+          } else {
+            connect()
+            .then(() => console.log('connected'))
+            .catch((e) => console.error(e))
+          }
+        }
+        } disabled={isFlipping}>
           {isConnected
             ? isFlipping
               ? 'Flipping...'
               : 'FLIP COIN'
-            : 'Connect Wallet'}
+            : 'Connect Wallet'
+          }
         </button>
         {result && <p className="result-text">RESULT: {result.toUpperCase()}</p>}
       </div>
